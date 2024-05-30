b6e9d18e5f42343452f89813cab377a3c559c921
jacquelinezakel <jacquelinezakel@gmail.com>
2024-05-30 10:32:22 +0200

    Doku zu den neuen Methoden und Einrückungen

--- main/swift-library/Sources/swift-library/SecureEnclaveManager.swift
+++ main/swift-library/Sources/swift-library/SecureEnclaveManager.swift
@@ -3,17 +3,17 @@ import LocalAuthentication
 import Security
 import CryptoKit
     
-    /*
-     Creates a new cryptographic key pair in the Secure Enclave.
+    /**
+    Creates a new cryptographic key pair in the Secure Enclave.
      
-     # Arguments
+    # Arguments
      
-     * 'privateKeyName' - A String used to identify the private key.
+    * 'privateKeyName' - A String used to identify the private key.
      
-     # Returns
+    # Returns
      
-     A 'SEKeyPair' containing the public and private keys on success, or a 'SecureEnclaveError' on failure.
-     */
+    A 'SEKeyPair' containing the public and private keys on success, or a 'SecureEnclaveError' on failure.
+    **/
     func create_key(privateKeyName: String ) throws -> SEKeyPair? {
         let accessControl = createAccessControlObject()
         
@@ -49,27 +49,35 @@ import CryptoKit
     }
 
 
-/** Optimized method off @generateKeyPair() to communicate with the rust-side abstraction-layer.
- Output and input is only in string-forms possible **/
-func rustcall_create_key(privateKeyName: RustString) -> String {
+    /** 
+    Optimized method off @create_key() to communicate with the rust-side abstraction-layer.
+
+    # Arguments
+
+    * 'privateKeyName' - A 'RustString' data type used to identify the private key.
+
+    # Returns
+
+    A String representing the private and public key.
+    **/
+    func rustcall_create_key(privateKeyName: RustString) -> String {
     // Add-Error-Case: If an Secure Enclave Processor does not exist.
-    do{
-        let keyPair = try create_key(privateKeyName: privateKeyName.toString())
-        return ("Private Key: "+String((keyPair?.privateKey.hashValue)!) + "\nPublic Key: " + String((keyPair?.publicKey.hashValue)!))
-    }catch{
-        return ("\(error)")
+        do{
+            let keyPair = try create_key(privateKeyName: privateKeyName.toString())
+            return ("Private Key: "+String((keyPair?.privateKey.hashValue)!) + "\nPublic Key: " + String((keyPair?.publicKey.hashValue)!))
+        }catch{
+            return ("\(error)")
+        }
     }
-}
     
     
-    /*
-     Creates an access control object for a cryptographic operation.
+    /**
+    Creates an access control object for a cryptographic operation.
      
+    #Returns
      
-     #Returns
-     
-     A 'SecAccessControl' configured for private key usage.
-     */
+    A 'SecAccessControl' configured for private key usage.
+    **/
     func createAccessControlObject() -> SecAccessControl {
         let access = SecAccessControlCreateWithFlags(
             kCFAllocatorDefault,
@@ -80,19 +88,19 @@ func rustcall_create_key(privateKeyName: RustString) -> String {
     }
     
     
-    /*
-     Encrypts data using a public key.
+    /**
+    Encrypts data using a public key.
      
-     # Arguments
+    # Arguments
      
-     * 'data' - Data that has to be encrypted.
+    * 'data' - Data that has to be encrypted.
      
-     * 'publicKey' - A SecKey data type of the Security Framework representing a cryptographic public key.
+    * 'publicKey' - A SecKey data type representing a cryptographic public key.
      
-     # Returns
+    # Returns
      
-     Data that has been encrypted on success, or a 'SecureEnclaveError' on failure.
-     */
+    Data that has been encrypted on success, or a 'SecureEnclaveError' on failure.
+    **/
     func encrypt_data(data: Data, publicKeyName: SecKey) throws -> Data {
         let algorithm = SecKeyAlgorithm.eciesEncryptionCofactorVariableIVX963SHA256AESGCM
         var error: Unmanaged<CFError>?
@@ -105,33 +113,47 @@ func rustcall_create_key(privateKeyName: RustString) -> String {
         return result! as Data
     }
 
-    func rustcall_encrypt_data(data: RustString, publicKeyName: RustString) -> String {
-    do{
-        let privateKey: SecKey = try load_key(key_id: publicKeyName.toString())!
-        let publicKey = getPublicKeyFromPrivateKey(privateKey: privateKey)
-        let encryptedData: Data = try encrypt_data(data: data.toString().data(using: String.Encoding.utf8)!, publicKeyName: publicKey!)
-        let encryptedData_string = encryptedData.base64EncodedString()
-        return ("\(encryptedData_string)")
-    }catch{
-        return ("\(error)")
-    }
 
-}
+    /** 
+    Optimized method off @encrypt_data() to communicate with the rust-side abstraction-layer.
+
+    # Arguments
+
+    * 'data' - A 'RustString' data type used to represent the data that has to be encrypted as a String.
+
+    * 'privateKeyName' - A 'RustString' data type used to identify the private key.
+
+    # Returns
+
+    A String representing the encrypted data.
+    **/
+    func rustcall_encrypt_data(data: RustString, publicKeyName: RustString) -> String {
+        do{
+            let privateKey: SecKey = try load_key(key_id: publicKeyName.toString())!
+            let publicKey = getPublicKeyFromPrivateKey(privateKey: privateKey)
+            let encryptedData: Data = try encrypt_data(data: data.toString().data(using: String.Encoding.utf8)!, publicKeyName: publicKey!)
+            let encryptedData_string = encryptedData.base64EncodedString()
+            return ("\(encryptedData_string)")
+        }catch{
+            return ("\(error)")
+        }
+
+    }
     
     
-    /*
-     Decrypts data using a private key.
+    /**
+    Decrypts data using a private key.
      
-     # Arguments
+    # Arguments
      
-     * 'data' - Encrypted data that has to be decrypted.
+    * 'data' - Encrypted data that has to be decrypted.
      
-     * 'privateKey' - A SecKey data type of the Security Framework representing a cryptographic private key.
+    * 'privateKey' - A SecKey data type representing a cryptographic private key.
      
-     # Returns
+    # Returns
      
-     Data that has been decrypted on success, or a 'SecureEnclaveError' on failure.
-     */
+    Data that has been decrypted on success, or a 'SecureEnclaveError' on failure.
+    **/
     func decrypt_data(data: Data, privateKey: SecKey) throws -> Data {
         let algorithm: SecKeyAlgorithm = SecKeyAlgorithm.eciesEncryptionCofactorVariableIVX963SHA256AESGCM
         var error: Unmanaged<CFError>?
@@ -144,6 +166,19 @@ func rustcall_create_key(privateKeyName: RustString) -> String {
         return result! as Data
     }
 
+    /** 
+    Optimized method off @decrypt_data() to communicate with the rust-side abstraction-layer.
+
+    # Arguments
+
+    * 'data' - A 'RustString' data type used to represent the data that has to be decrypted as a String.
+
+    * 'privateKeyName' - A 'RustString' data type used to identify the private key.
+
+    # Returns
+
+    A String representing the decrypted data.
+    **/
     func rustcall_decrypt_data(data: RustString, privateKeyName: RustString) -> String{
         do{
             guard let data = Data(base64Encoded: data.toString())
@@ -164,36 +199,36 @@ func rustcall_create_key(privateKeyName: RustString) -> String {
     
     
     
-    /*
-     Retrieves the public key associated with a given private key.
+    /**
+    Retrieves the public key associated with a given private key.
      
-     # Arguments
+    # Arguments
      
-     * 'privateKey' - A SecKey data type of the Security Framework representing a cryptographic private key.
+    * 'privateKey' - A SecKey data type representing a cryptographic private key.
      
-     # Returns
+    # Returns
      
-     Optionally a public key of the Security Framework representing a cryptographic public key on success, or 'nil' on failure
+    Optionally a public key representing a cryptographic public key on success, or 'nil' on failure
      
-     */
+    **/
     func getPublicKeyFromPrivateKey(privateKey: SecKey) -> SecKey? {
         return SecKeyCopyPublicKey(privateKey)
     }
     
     
-    /*
-     Signs data using a private key.
+    /**
+    Signs data using a private key.
      
-     # Arguments
+    # Arguments
      
-     * 'privateKey' - A SecKey data type of the Security Framework representing a cryptographic private key.
+    * 'privateKey' - A SecKey data type representing a cryptographic private key.
      
-     * 'content' - A CFData data type of the Core Foundation that has to be signed.
+    * 'content' - A CFData data type of the Core Foundation that has to be signed.
      
-     # Returns
+    # Returns
      
-     Optionally data that has been signed as a CFData data type on success, or 'nil' on failure.
-     */
+    Optionally data that has been signed as a CFData data type on success, or 'nil' on failure.
+    **/
     func sign_data(data: CFData, privateKeyReference: SecKey) throws -> CFData? {
         let sign_algorithm = SecKeyAlgorithm.ecdsaSignatureMessageX962SHA256;
         if !SecKeyIsAlgorithmSupported(privateKeyReference, SecKeyOperationType.sign, sign_algorithm){
@@ -209,6 +244,19 @@ func rustcall_create_key(privateKeyName: RustString) -> String {
     }
     
 
+    /** 
+    Optimized method off @sign_data() to communicate with the rust-side abstraction-layer.
+
+    # Arguments
+
+    * 'data' - A 'RustString' data type used to represent the data that has to be signed as a String.
+
+    * 'privateKeyName' - A 'RustString' data type used to identify the private key.
+
+    # Returns
+
+    A String representing the signed data.
+    **/
     func rustcall_sign_data(data: RustString, privateKeyName: RustString) -> String{
         let privateKeyName_string = privateKeyName.toString()
         let data_cfdata = data.toString().data(using: String.Encoding.utf8)! as CFData
@@ -223,22 +271,21 @@ func rustcall_create_key(privateKeyName: RustString) -> String {
     }
     
     
-    /*
-     Verifies a signature using a public key.
+    /**
+    Verifies a signature using a public key.
      
-     # Arguments
+    # Arguments
      
-     * 'publicKey - A SecKey data type of the Security Framework representing a cryptographic public key.
+    * 'publicKey - A SecKey data type representing a cryptographic public key.
      
-     * 'content' - A String of the data that has to be verified.
+    * 'content' - A String of the data that has to be verified.
      
-     * 'signature' - A CFData data type of the Core Foundation that is the signature.
+    * 'signature' - A CFData data type of the Core Foundation that is the signature.
      
-     # Returns
+    # Returns
      
-     A boolean if the signature is valid on success, or a 'SecureEnclaveError' on failure.
-     */
-
+    A boolean if the signature is valid on success, or a 'SecureEnclaveError' on failure.
+    **/
     func verify_signature(publicKey: SecKey, data: String, signature: String) throws -> Bool {
         let sign_algorithm = SecKeyAlgorithm.ecdsaSignatureMessageX962SHA256
         guard Data(base64Encoded: signature) != nil else{
@@ -258,6 +305,22 @@ func rustcall_create_key(privateKeyName: RustString) -> String {
         }
     }
 
+
+    /** 
+    Optimized method off @verify_data() to communicate with the rust-side abstraction-layer.
+
+    # Arguments
+
+    * 'data' - A 'RustString' data type used to represent the data that has to be verified as a String.
+
+    * 'signature' - A 'RustString' data type used to represent the signature of the signed data as a String.
+
+    * 'publicKeyName' - A 'RustString' data type used to identify the public key.
+
+    # Returns
+
+    A String if the data could have been verified with the signature.
+    **/
     func rustcall_verify_data(data: RustString, signature: RustString, publicKeyName: RustString) -> String{
         do{
             let publicKeyName_string = publicKeyName.toString()
@@ -293,17 +356,17 @@ func rustcall_create_key(privateKeyName: RustString) -> String {
     }
     
     
-    /*
-     Loads a cryptographic key from the keychain.
+    /**
+    Loads a cryptographic private key from the keychain.
      
-     # Arguments
+    # Arguments
      
-     * 'key_id' - A String used as the identifier for the key
+    * 'key_id' - A String used as the identifier for the key
      
-     # Returns
+    # Returns
      
-     Otionally the key as a SecKey data type on success, or a 'SecureEnclaveError' on failure.
-     */
+    Optionally the key as a SecKey data type on success, or a 'SecureEnclaveError' on failure.
+    **/
     func load_key(key_id: String) throws -> SecKey? {
         let tag = key_id
         let query: [String: Any] = [
@@ -321,31 +384,43 @@ func rustcall_create_key(privateKeyName: RustString) -> String {
         return (item as! SecKey)
     }
 
+
+    /** 
+    Optimized method off @load_key() to communicate with the rust-side abstraction-layer.
+
+    # Arguments
+
+    * 'keyID' - A 'RustString' data type used to represent identifier for the key as a String.
+
+    # Returns
+
+    A String representing the private key as a String.
+    **/
     func rustcall_load_key(keyID: RustString) -> String {
-    do {
-        guard let key = try load_key(key_id: keyID.toString()) else {
-            return "Key not found."
+        do {
+            guard let key = try load_key(key_id: keyID.toString()) else {
+                return "Key not found."
+            }
+            return "\(key.hashValue)"
+        } catch {
+            return "\(error)"
         }
-        return "\(key.hashValue)"
-    } catch {
-        return "\(error)"
     }
-}
     
     
-    /*
-     Stores a cryptographic key in the keychain.
+    /**
+    Stores a cryptographic key in the keychain.
      
-     # Arguments
+    # Arguments
      
-     * 'name' - A String used to identify the key in the keychain.
+    * 'name' - A String used to identify the key in the keychain.
      
-     * 'privateKey' - A SecKey data type of the Security Framework representing a cryptographic private key.
+    * 'privateKey' - A SecKey data type representing a cryptographic private key.
      
-     # Returns
+    # Returns
      
-     A 'SecureEnclaveError' on failure.
-     */
+    A 'SecureEnclaveError' on failure.
+    **/
     func storeKey_Keychain(_ name: String, _ privateKey: SecKey) throws {
         let key = privateKey
         let tag = name.data(using: .utf8)!
@@ -364,13 +439,13 @@ func rustcall_create_key(privateKeyName: RustString) -> String {
     //     return true
     // }
     
-    /*
-     Inizializes a module by creating a private key and the associated private key.
+    /**
+    Inizializes a module by creating a private key and the associated private key. Optimized to communicate with the rust-side abstraction-layer.
      
-     # Returns
+    # Returns
      
-     A boolean if the module has been inizializes correctly on success, or a 'SecureEnclaveError' on failure.
-     */
+    A boolean if the module has been inizializes correctly on success, or a 'SecureEnclaveError' on failure.
+    **/
     func initializeModule() -> Bool  {
         if #available(macOS 10.15, *) {
             var initialized: Bool = true
